5f84065ca85919d8cbc497ec79ccca75b855cdc1
Elham Mousavi <elham.msv531@gmail.com>
2023-09-01 02:18:16 -0700

    new search box

---
+++ search box/project2/script.js
@@ -0,0 +1,11 @@
+
+let search_icon = document.getElementById("search-icon");
+let search=document.getElementById("search")
+
+
+
+
+
+search_icon.onclick=function (){
+    search.classList.toggle("open")
+}
